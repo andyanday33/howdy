05d3bd7bf65da62451ec3411565bee48bb727c4b
andyanday33 <andyanday33@gmail.com>
2021-07-04 17:39:02 +0300

    updates on conversation screen

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:howdy/widgets/appBar.dart';
 
 class ConversationScreen extends StatefulWidget {
   const ConversationScreen({Key? key}) : super(key: key);
@@ -8,8 +9,52 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  TextEditingController messagetec = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: mainAppBar(context),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.white12,
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                          controller: messagetec,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Send a message...",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: InputBorder.none,
+                          )),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [Colors.white30, Colors.white38]),
+                            borderRadius: BorderRadius.circular(40)),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(Icons.message_rounded),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
